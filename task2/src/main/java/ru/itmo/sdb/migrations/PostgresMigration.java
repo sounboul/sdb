c3eeebf99eed51dd7c144669475aa6aa42ecb23e
Andrey Bova <andrey.bova@team.wrike.com>
2020-05-25 04:33:03 +0300

    Add postgres migration

---
+++ task2/src/main/java/ru/itmo/sdb/migrations/PostgresMigration.java
@@ -0,0 +1,162 @@
+package ru.itmo.sdb.migrations;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.itmo.sdb.migrations.conflicts.MigrationReport;
+import ru.itmo.sdb.migrations.conflicts.Source;
+import ru.itmo.sdb.models.Discipline;
+import ru.itmo.sdb.models.Person;
+import ru.itmo.sdb.models.Specialty;
+import ru.itmo.sdb.models.Student;
+import ru.itmo.sdb.models.StudyGroup;
+import ru.itmo.sdb.models.Subject;
+import ru.itmo.sdb.models.Teacher;
+import ru.itmo.sdb.models.University;
+import ru.itmo.sdb.repositories.StoreRepositoryHelper;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class PostgresMigration {
+    private ru.itmo.sdb.postgres.core.repositories.StudentRepository postgresStudentRepository;
+    private ru.itmo.sdb.postgres.core.repositories.SubjectRepository postgresSubjectRepository;
+    private ru.itmo.sdb.postgres.core.repositories.TeacherRepository postgresTeacherRepository;
+    private ru.itmo.sdb.postgres.core.repositories.DisciplineRepository postgresDisciplineRepository;
+    private ru.itmo.sdb.postgres.core.repositories.SpecialtyRepository postgresSpecialtyRepository;
+    private ru.itmo.sdb.postgres.core.repositories.PersonRepository postgresPersonRepository;
+    private ru.itmo.sdb.postgres.core.repositories.UniversityRepository postgresUniversityRepository;
+
+    private StoreRepositoryHelper repositoryHelper;
+
+    @Autowired
+    public PostgresMigration(
+            ru.itmo.sdb.postgres.core.repositories.StudentRepository studentRepository,
+            ru.itmo.sdb.postgres.core.repositories.SubjectRepository subjectRepository,
+            ru.itmo.sdb.postgres.core.repositories.TeacherRepository teacherRepository,
+            ru.itmo.sdb.postgres.core.repositories.DisciplineRepository disciplineRepository,
+            ru.itmo.sdb.postgres.core.repositories.SpecialtyRepository specialtyRepository,
+            ru.itmo.sdb.postgres.core.repositories.PersonRepository personRepository,
+            ru.itmo.sdb.postgres.core.repositories.UniversityRepository universityRepository,
+            StoreRepositoryHelper repositoryHelper) {
+        this.postgresStudentRepository = studentRepository;
+        this.postgresSubjectRepository = subjectRepository;
+        this.postgresTeacherRepository = teacherRepository;
+        this.postgresDisciplineRepository = disciplineRepository;
+        this.postgresSpecialtyRepository = specialtyRepository;
+        this.postgresPersonRepository = personRepository;
+        this.postgresUniversityRepository = universityRepository;
+        this.repositoryHelper = repositoryHelper;
+    }
+
+    public MigrationReport migrate() {
+        Map<Long, University> universities = new HashMap<>();
+        postgresUniversityRepository.findAll()
+                .forEach(x -> {
+                    University university = new University();
+                    university.setName(x.getName());
+                    universities.put(x.getId(), university);
+                });
+
+        Map<Long, Discipline> disciplines = new HashMap<>();
+        postgresDisciplineRepository.findAll()
+                .forEach(x -> {
+                    Discipline discipline = new Discipline();
+
+                    discipline.setName(x.getName());
+                    discipline.setPractices(x.getPractices());
+                    discipline.setLectures(x.getLectures());
+                    discipline.setLabs(x.getLabs());
+                    discipline.setControlForm(x.getControlForm());
+
+                    disciplines.put(x.getId(), discipline);
+                });
+
+        Map<Long, Person> people = new HashMap<>();
+        postgresPersonRepository.findAll()
+                .forEach(x -> {
+                    Person person = new Person();
+
+                    person.setFirstName(x.getFirstName());
+                    person.setLastName(x.getLastName());
+                    person.setPatronymic(x.getPatronymic());
+                    person.setBirthDate(x.getBirthDate());
+
+                    people.put(x.getId(), person);
+                });
+
+        Map<Long, Specialty> specialties = new HashMap<>();
+        postgresSpecialtyRepository.findAll()
+                .forEach(x -> {
+                    Specialty specialty = new Specialty();
+
+                    specialty.setName(x.getName());
+                    specialty.setUniversity(universities.get(x.getUniversity()
+                                                                     .getId()));
+
+                    specialties.put(x.getId(), specialty);
+                });
+
+        Map<Long, Student> students = new HashMap<>();
+        List<StudyGroup> studyGroups = new ArrayList<>();
+        postgresStudentRepository.findAll()
+                .forEach(x -> {
+                    Student student = new Student();
+                    student.setPerson(people.get(x.getPerson()
+                                                         .getId()));
+
+                    StudyGroup studyGroup = new StudyGroup();
+                    studyGroup.setName(x.getStudyGroup());
+                    studyGroup.setSpecialty(specialties.get(x.getSpecialty()
+                                                                    .getId()));
+
+                    student.setStudyGroup(studyGroup);
+                    studyGroups.add(studyGroup);
+                    students.put(x.getId(), student);
+                });
+
+        Map<Long, Teacher> teachers = new HashMap<>();
+        postgresTeacherRepository.findAll()
+                .forEach(x -> {
+                    Teacher teacher = new Teacher();
+
+                    teacher.setPerson(people.get(x.getPerson()
+                                                         .getId()));
+                    teachers.put(x.getId(), teacher);
+                });
+
+        Map<Long, Subject> subjects = new HashMap<>();
+        postgresSubjectRepository.findAll()
+                .forEach(x -> {
+                    Subject subject = new Subject();
+
+                    subject.setTeacher(teachers.get(x.getTeacher()
+                                                            .getId()));
+                    subject.setDiscipline(disciplines.get(x.getDiscipline()
+                                                                  .getId()));
+                    subject.setSpecialty(specialties.get(x.getSpecialty()
+                                                                 .getId()));
+                    subject.setYear(x.getYear());
+                    subject.setMaxPoints(x.getMaxPoints());
+                    subject.setTerm(x.getTerm());
+
+                    subjects.put(x.getId(), subject);
+                });
+
+
+        return new MigrationReport(Source.POSTGRES, Arrays.asList(
+                repositoryHelper.saveWithReport(universities.values(), University.class),
+                repositoryHelper.saveWithReport(disciplines.values(), Discipline.class),
+                repositoryHelper.saveWithReport(people.values(), Person.class),
+                repositoryHelper.saveWithReport(specialties.values(), Specialty.class),
+                repositoryHelper.saveWithReport(studyGroups, StudyGroup.class),
+                repositoryHelper.saveWithReport(students.values(), Student.class),
+                repositoryHelper.saveWithReport(teachers.values(), Teacher.class),
+                repositoryHelper.saveWithReport(subjects.values(), Subject.class)
+        ));
+    }
+}
